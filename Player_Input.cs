af5848f9704bc1830feac29794636239ff508e22
ofek-droid <67185223+ofek-droid@users.noreply.github.com>
2020-10-16 23:24:41 +0300

    Add files via upload

---
+++ Player_Input.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Player_Input : MonoBehaviour
+{
+    public float maxJumpTime = 0.75f;
+    float gravity = 0;
+    public float gravityScale = -9.8f;
+    Vector3 movement;
+    public float speed = 10f;
+    CharacterController controller;
+    public float jumpHeight = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        movement.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        movement.z = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+
+
+        controller.Move(movement);
+        
+        
+
+    }
+
+    
+
+    
+}
---
+++ Player_Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject groundChecker;
+    public float radius = 0.31f;
+    public LayerMask whatIsGround;
+
+
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    float jump = 8.0f;
+    CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Physics.CheckSphere(groundChecker.transform.position, radius, whatIsGround))
+        {
+            if (Input.GetButton("Jump"))
+            {
+                Debug.Log("Hi");
+                jump = jumpSpeed;
+            }
+
+        }
+        jump -= gravity * Time.deltaTime;
+        controller.Move(jump * Time.deltaTime * Vector3.up);
+    }
+}
